0fc35181a18bc6be9136b5d8619ce2323b359500
Imperial-Industries <shivansh.gour90@gmail.com>
2021-02-11 12:32:04 -0500

    reboilerplating

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from icecream import ic as println # Used for custom outputs
+import os
+import sys
 from encryption import *
 println.configureOutput(prefix='Neutron > ', includeContext=False) # Setting up custom outputs
-sk = input("Neutron > SK (h for help): ") # Neutron Start Key, determining what function Neutron leads to (0, 1, 2, h)
-if sk == "h":
-     println("SK 0 is used to access the email client")
-     println("SK 1 is used to access the password manager")
-     println("SK 2 is used to access the encryption machine")
-elif sk == "0":
-     println(sk)
-elif sk == "1":
-     println(sk)
-elif sk == "2":
-     println(sk)
-     operation = input("Neutron > Operation: ")
-
-     string = input("Neutron > String: ")
-     println(encrypt(string))
-else:
-     println("Error")
+def init():
+     SK = input("Neutron > SK (h for help): ") # Neutron Start Key, determining what function Neutron leads to (0, 1, 2, h)
+     if SK == "h":
+          println("SK 0 is used to access the email client")
+          println("SK 1 is used to access the password manager")
+          println("SK 2 is used to access the encryption machine")
+          init()
+     elif SK == "0":
+          println(SK)
+     elif SK == "1":
+          println(SK)
+     elif SK == "2":
+          println(SK)
+          ok = input("Neutron > OK (h for help): ")
+          if ok == "h":
+               println("OK 0 is used to encrypt messages.")
+               println("OK 1 is used to decrypt messages.")
+          elif ok == "0":
+               String = input("Neutron > String: ")
+               println(encrypt(String))
+     else:
+          println("Error: Invalid Neutron SK. Type h for help.")
+          init()
+init()
