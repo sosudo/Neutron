246ae87cb7e7870ad097c8cf05652d0a0739fe15
Imperial-Industries <shivansh.gour90@gmail.com>
2021-02-11 15:10:23 -0500

    encryption complete

--- encryption.py
+++ encryption.py
@@ -83,12 +83,23 @@ alphanum = {
      "?":76,
      ">":78,
      ",":58,
-     "<":55
+     "<":55,
+     "1":"?",
+     "2":"]",
+     "3":"/",
+     "4":"$",
+     "5":"*",
+     "6":"(",
+     "7":")",
+     "8":"-",
+     "9":"@",
+     "0":"_",
+     " ":"+"
 }
 def Encrypt(string:str) -> str:
       encrypted_string = string
       while encrypted_string==string:
-            shift_value = random.randint(-1000,1000)
+            shift_value = random.randint(-10000000,10000000)
             if shift_value < 0:
                   encrypted_string = lcirc(shift_value, string)
             else:
@@ -96,5 +107,14 @@ def Encrypt(string:str) -> str:
       encrypted_list = [i for i in encrypted_string]
       encrypted_string = ''
       for i in encrypted_list:
-            encrypted_string += str(alphanum[i])+"." if i != " " else "."
+            encrypted_string += str(alphanum[i])+"."
+      symbol = random.choice(["!","#","%","^","&","=","`","~"])
+      encrypted_list = [i for i in encrypted_string]
+      for i in range(len(encrypted_list)):
+            if i%2 == 0:
+                  encrypted_list.insert(i,symbol)
+      encrypted_string = ''
+      for i in encrypted_list:
+            encrypted_string += str(alphanum[i])+"."
+      encrypted_string += "|"+str(shift_value)+"|"+symbol
       return encrypted_string
