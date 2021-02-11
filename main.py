78b7a364831525ec39ce29325d54a4f0741d4a70
Imperial-Industries <shivansh.gour90@gmail.com>
2021-02-10 23:33:18 -0500

    boilerplate

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-from icecream import ic as println
-println.configureOutput(prefix='Neutron ', includeContext=False)
+from icecream import ic as println # Used for custom outputs
+println.configureOutput(prefix='Neutron > ', includeContext=False) # Setting up custom outputs
+sk = input("Neutron SK (h for help): ") # Neutron Start Key, determining what function Neutron leads to (0, 1, 2, h)
+if sk == "h":
+     println("SK 0 is used to access the email client")
+     println("SK 1 is used to access the password manager")
+     println("SK 2 is used to access the encryption machine")
+elif sk == "0":
+     println(sk)
+elif sk == "1":
+     println(sk)
+elif sk == "2":
+     println(sk)
+else:
+     println("Error")
