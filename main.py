36355513d716c3ff62c19f11922a9126cd0fb7e1
Imperial-Industries <shivansh.gour90@gmail.com>
2021-02-11 12:06:00 -0500

    encrypt2

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from icecream import ic # Used for custom outputs
-from icecream import install  # Used to install icecream project wide
+from icecream import ic as println # Used for custom outputs
 from encryption import *
-ic.configureOutput(prefix='Neutron > ', includeContext=False) # Setting up custom outputs
-install()
+println.configureOutput(prefix='Neutron > ', includeContext=False) # Setting up custom outputs
 sk = input("Neutron > SK (h for help): ") # Neutron Start Key, determining what function Neutron leads to (0, 1, 2, h)
 if sk == "h":
-     ic("SK 0 is used to access the email client")
-     ic("SK 1 is used to access the password manager")
-     ic("SK 2 is used to access the encryption machine")
+     println("SK 0 is used to access the email client")
+     println("SK 1 is used to access the password manager")
+     println("SK 2 is used to access the encryption machine")
 elif sk == "0":
-     ic(sk)
+     println(sk)
 elif sk == "1":
-     ic(sk)
+     println(sk)
 elif sk == "2":
-     ic(sk)
+     println(sk)
+     operation = input("Neutron > Operation: ")
+
      string = input("Neutron > String: ")
-     ic(encrypt(string))
+     println(encrypt(string))
 else:
-     ic("Error")
+     println("Error")
